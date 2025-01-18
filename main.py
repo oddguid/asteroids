c05d62feed6d7064ea64c99344923858eb72e790
oddguid <george.mathijssen@gmail.com>
2025-01-18 20:35:36 +0100

    Minimal pygame program

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+from constants import *
+
+def main() -> int:
+    print("Starting asteroids!")
+
+    # Initialize pygame
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    sys.exit(main())
+
