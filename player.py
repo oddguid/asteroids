666c2e22fb57eab4e163e97302de54ad7a76a9c3
oddguid <george.mathijssen@gmail.com>
2025-01-20 20:49:35 +0100

    Added shot cooldown timer to prevent Zerg rush

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown_timer = 0
 
     # in the player class
     def triangle(self):
@@ -30,6 +31,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        self.cooldown_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -44,7 +47,12 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.cooldown_timer > 0:
+            return
+
         shot = Shot(self.position.x, self.position.y)
 
         velocity = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = velocity * PLAYER_SHOOT_SPEED
+        
+        self.cooldown_timer = PLAYER_SHOOT_COOLDOWN
